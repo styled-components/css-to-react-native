1bc3e7f205e8f33532931f6f725a6f0501eb5c45
taoqf <tao_qiufeng@126.com>
2019-03-26 16:23:13 +0800

    add types

---
+++ index.d.ts
@@ -0,0 +1,8 @@
+export interface Style {
+	[key: string]: string;
+}
+
+export function getPropertyName(name: string): string;
+export function getStylesForProperty(name: string, value: string): Style;
+
+export default function transform(css: Array<[string, string]>, output?: string[], getStylesForProperty?: boolean): Style;
