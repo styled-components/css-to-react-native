983f85ea2f7c747c4f7e87f8706556a4e15037b4
krister <krister@sent.at>
2019-08-14 11:10:00 +0300

    Fix border-color only accepting named colors

--- src/__tests__/borderColor.js
+++ src/__tests__/borderColor.js
@@ -8,3 +8,30 @@ it('transforms border color with multiple values', () => {
     borderLeftColor: 'blue',
   })
 })
+
+it('transforms border color with hex color', () => {
+  expect(transformCss([['border-color', '#f00']])).toEqual({
+    borderBottomColor: '#f00',
+    borderLeftColor: '#f00',
+    borderRightColor: '#f00',
+    borderTopColor: '#f00',
+  })
+})
+
+it('transforms border color with rgb color', () => {
+  expect(transformCss([['border-color', 'rgb(255, 0, 0)']])).toEqual({
+    borderBottomColor: 'rgb(255, 0, 0)',
+    borderLeftColor: 'rgb(255, 0, 0)',
+    borderRightColor: 'rgb(255, 0, 0)',
+    borderTopColor: 'rgb(255, 0, 0)',
+  })
+})
+
+it('transforms border color with rgba color', () => {
+  expect(transformCss([['border-color', 'rgba(255, 0, 0, 0.1)']])).toEqual({
+    borderBottomColor: 'rgba(255, 0, 0, 0.1)',
+    borderLeftColor: 'rgba(255, 0, 0, 0.1)',
+    borderRightColor: 'rgba(255, 0, 0, 0.1)',
+    borderTopColor: 'rgba(255, 0, 0, 0.1)',
+  })
+})
